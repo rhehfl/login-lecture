73bbed1fbe3e7ced02bf0cd30e987632dab2770a
GuDoYoon <rhehfl418q@gmail.com>
2024-06-26 14:54:15 +0900

    폴더 구조 최적화

--- app/app.js
+++ app/app.js
@@ -4,9 +4,9 @@ const express = require("express");
 const app = express();
 
 //라우팅
-const home = require("./routes/home"); //라우팅한것(index.js)파일에 접근
+const home = require("./src/routes/home"); //라우팅한것(index.js)파일에 접근
 //앱 세팅
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.use("/", home);
