c1f7516be71cc903619696ed85da000e5ccf8967
GuDoYoon <rhehfl418q@gmail.com>
2024-06-27 11:19:18 +0900

    로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -7,13 +7,25 @@ class UserStorage {
   };
   static getUsers(...fields) {
     const users = this.#users;
+
     const newUsers = fields.reduce((newUsers, field) => {
       if (users.hasOwnProperty(field)) {
         newUsers[field] = users[field];
       }
       return newUsers;
     }, {});
+
     return newUsers;
+  }
+  static getUserInfo(id) {
+    const users = this.#users;
+    const idx = users.id.indexOf(id);
+    const userInfo = Object.keys(users).reduce((newUser, info) => {
+      newUser[info] = users[info][idx];
+      return newUser;
+    }, {});
+
+    return userInfo;
   }
 }
 module.exports = UserStorage;
